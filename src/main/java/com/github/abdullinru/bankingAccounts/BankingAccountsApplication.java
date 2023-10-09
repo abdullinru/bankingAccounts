3d6000dffad36d0bd120e93b3ea2453acd60a9f7
abdullinru <abdullin17021989@gmail.com>
2023-10-09 23:02:32 +0600

    init commit

---
+++ src/main/java/com/github/abdullinru/bankingAccounts/BankingAccountsApplication.java
@@ -0,0 +1,13 @@
+package com.github.abdullinru.bankingAccounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAccountsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAccountsApplication.class, args);
+	}
+
+}
