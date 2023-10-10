ea0810bccdb92819127ad17cc3423a9f75e58cbd
abdullinru <abdullin17021989@gmail.com>
2023-10-11 01:27:29 +0600

    бизнес логика функций депозита, снятия и перевода денег, создания аккаунта, получения всех аккаунтов

--- src/main/java/com/github/abdullinru/bankingAccounts/service/AccountService.java
+++ src/main/java/com/github/abdullinru/bankingAccounts/service/AccountService.java
@@ -1,29 +1,125 @@
 package com.github.abdullinru.bankingAccounts.service;
 
 import com.github.abdullinru.bankingAccounts.dto.*;
+import com.github.abdullinru.bankingAccounts.exception.AccountNotFoundException;
+import com.github.abdullinru.bankingAccounts.mapper.AccountMapper;
+import com.github.abdullinru.bankingAccounts.model.Account;
+import com.github.abdullinru.bankingAccounts.repository.AccountRepository;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class AccountService {
-    public List<ResponseAccountDto> getAllAccounts() {
-        return null;
+    private final AccountRepository accountRepository;
+    private final AccountMapper mapper;
+
+    public AccountService(AccountRepository accountRepository, AccountMapper mapper) {
+        this.accountRepository = accountRepository;
+        this.mapper = mapper;
+    }
+
+    public List<ResponseAccountDto> getAllAccounts(int page, int size) {
+        checkPageAndSize(page, size);
+        PageRequest pageRequest = PageRequest.of(page-1, size);
+        List<Account> accounts = accountRepository.findAll(pageRequest).getContent();
+
+        return mapper.toListResponseAccountDto(accounts);
+    }
+
+    private void checkPageAndSize(int page, int size) {
+        if (page <= 0 || size <= 0) {
+            throw new IllegalArgumentException("incorrect value page or size");
+        }
     }
 
     public ResponseAccountDto createAccount(RequestAccountDto accountDto) {
-        return null;
+        checkBalance(accountDto.getBalance());
+        checkPin(accountDto.getPinCode());
+        Account createAccount = mapper.toAccount(accountDto);
+        accountRepository.save(createAccount);
+        ResponseAccountDto response = mapper.toResponseAccountDto(createAccount);
+        return response;
+    }
+
+    private void checkPin(String pinCode) {
+        if (!pinCode.matches("\\d{4}")) {
+            throw new IllegalArgumentException("incorrect pincode");
+        }
+    }
+
+    private void checkBalance(double balance) {
+        if (balance < 0) {
+            throw new IllegalArgumentException("Balance is less then 0");
+        }
     }
 
     public ResponseAccountDto deposit(DepositDto depositDto) {
-        return null;
+        chechAmount(depositDto.getAmount());
+        Account findAccount = accountRepository
+                .findById(depositDto.getAccountId())
+                .orElseThrow(AccountNotFoundException::new);
+        findAccount.setBalance(findAccount.getBalance() + depositDto.getAmount());
+        accountRepository.save(findAccount);
+        ResponseAccountDto response = mapper.toResponseAccountDto(findAccount);
+        return response;
+    }
+
+    private void chechAmount(double amount) {
+        if (amount < 0) {
+            throw new IllegalArgumentException("incorrect value of deposit");
+        }
     }
 
     public ResponseAccountDto withdraw(WithdrawDto withdrawDto) {
-        return null;
+        chechAmount(withdrawDto.getAmount());
+        checkPin(withdrawDto.getPinCode());
+        Account findAccount = accountRepository
+                .findById(withdrawDto.getAccountId())
+                .orElseThrow(AccountNotFoundException::new);
+
+        comparePinCodes(withdrawDto.getPinCode(), findAccount.getPinCode());
+        if (findAccount.getBalance() < withdrawDto.getAmount()) {
+            throw new IllegalArgumentException("not enouht money for withdraw");
+        }
+        findAccount.setBalance(findAccount.getBalance() - withdrawDto.getAmount());
+        accountRepository.save(findAccount);
+        ResponseAccountDto response = mapper.toResponseAccountDto(findAccount);
+        return response;
+    }
+
+    private void comparePinCodes(String pin1, String pin2) {
+        if (!pin1.equals(pin2)) {
+            throw new IllegalArgumentException("PIN codes do not match");
+        }
     }
 
     public ResponseAccountDto transfer(TransferDto transferDto) {
-        return null;
+
+        chechAmount(transferDto.getTransferAmount());
+        checkPin(transferDto.getPinCode());
+        Account findSender = accountRepository
+                .findById(transferDto.getSenderId())
+                .orElseThrow(AccountNotFoundException::new);
+
+        Account findRecipient = accountRepository
+                .findById(transferDto.getRecipientId())
+                .orElseThrow(AccountNotFoundException::new);
+
+        comparePinCodes(transferDto.getPinCode(), findSender.getPinCode());
+
+        if (findSender.getBalance() < transferDto.getTransferAmount()) {
+            throw new IllegalArgumentException("not enouht money for withdraw");
+        }
+        findSender.setBalance(findSender.getBalance() - transferDto.getTransferAmount());
+        findRecipient.setBalance(findRecipient.getBalance() + transferDto.getTransferAmount());
+        accountRepository.save(findRecipient);
+        accountRepository.save(findSender);
+        ResponseAccountDto response = mapper.toResponseAccountDto(findSender);
+        return response;
     }
 }
